9037afa3b9703e485fec10c80ed998d48b010854
Ayan Das <bvits@riseup.net>
2024-11-07 16:20:37 +0530

    update notes

---
+++ manifest.scm
@@ -0,0 +1,8 @@
+;; -*- mode: scheme; -*-
+(specifications->manifest
+  '("python"
+    "python-requests"
+    "python-fuzzywuzzy"
+    "python-pyalex"
+    "python-bibtexparser-1"
+    "python-pylatexenc"))
